44fa03e5e67357a87e383d30b61bbeab3c2cd671
denis-malyshev <denis.malyshev@teamdev.com>
2016-03-21 18:15:28 +0200

    test-branch - amend first commit

---
+++ src/main/java/com/teamdev/training/Main.java
@@ -0,0 +1,10 @@
+package com.teamdev.training;
+
+public class Main {
+
+    public static final String FIRST_COMMIT = "FIRST COMMIT";
+
+    public static void main(String[] args) {
+        System.out.println(FIRST_COMMIT);
+    }
+}
