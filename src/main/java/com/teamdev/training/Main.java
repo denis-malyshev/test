4b4772945e47a62646fb0b2f73ea181865e63792
denis-malyshev <denis.malyshev@teamdev.com>
2016-03-21 18:19:40 +0200

    test-branch - second commit

--- src/main/java/com/teamdev/training/Main.java
+++ src/main/java/com/teamdev/training/Main.java
@@ -3,8 +3,10 @@ package com.teamdev.training;
 public class Main {
 
     public static final String FIRST_COMMIT = "FIRST COMMIT";
+    public static final String SECOND_COMMIT = "SECOND COMMIT";
 
     public static void main(String[] args) {
         System.out.println(FIRST_COMMIT);
+        System.out.println(SECOND_COMMIT);
     }
 }
