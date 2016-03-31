e14ac17e65d7a324a61ea91bcf033e9960d93161
denis.malyshev <denis.malyshev@teamdev.com>
2016-03-31 10:49:59 +0300

    deleted commit

--- src/main/java/com/teamdev/training/Main.java
+++ src/main/java/com/teamdev/training/Main.java
@@ -3,12 +3,8 @@ package com.teamdev.training;
 public class Main {
 
     public static final String FIRST_COMMIT = "FIRST COMMIT";
-    public static final String SECOND_COMMIT = "SECOND COMMIT";
-    public static final String THIRD_COMMIT = "THIRD COMMIT";
 
     public static void main(String[] args) {
         System.out.println(FIRST_COMMIT);
-        System.out.println(SECOND_COMMIT);
-        System.out.println(THIRD_COMMIT);
     }
 }
